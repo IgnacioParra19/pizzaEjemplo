cd596952824b91f7569f4d086578f802c0040969
IgnacioParra19 <ignacioparraingaramo@gmail.com>
2024-04-14 18:46:21 -0300

    Tercera pizza con interface

---
+++ iPizzaBuilder.js
@@ -0,0 +1,29 @@
+// IPizzaBuilder interface (simulado con comentarios de documentación)
+class IPizzaBuilder {
+    /**
+     * Set the size of the pizza.
+     * @param {string} size - The size of the pizza.
+     */
+    setSize(size) {}
+  
+    /**
+     * Add cheese to the pizza.
+     */
+    addCheese() {}
+  
+    /**
+     * Add pepperoni to the pizza.
+     */
+    addPepperoni() {}
+  
+    /**
+     * Add tomato to the pizza.
+     */
+    addTomato() {}
+  
+    /**
+     * Build the pizza object.
+     * @returns {Pizza} The pizza object.
+     */
+    build() {}
+  }
--- pizza.js
+++ pizza.js
@@ -1,15 +1,13 @@
-
 class Pizza {
-  constructor() {
-    this.size = '';
-    this.cheese = false;
-    this.pepperoni = false;
-    this.tomato = false;
+  constructor(builder) {
+    this.size = builder.size;
+    this.cheese = builder.cheese || false;
+    this.pepperoni = builder.pepperoni || false;
+    this.tomato = builder.tomato || false;
   }
   
   describe() {
     console.log(`Pizza - Size: ${this.size}, Cheese: ${this.cheese}, Pepperoni: ${this.pepperoni}, Tomato: ${this.tomato}`);
   }
 }
-
 module.exports = Pizza;
